8949644065af66431a1503f42011c41570e53efc
beccanaul <beccanaul@gmail.com>
2016-09-22 20:37:38 -0700

    change hover text to animals

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // create variable to count mouse overs
 var mouseOverCount = 0;
 
+var arr = [
+  'fish', 'dog', 'cat'
+];
+
 // count when mouse enters div
 $( ".out" )
-  .mouseover(function() {
-    mouseOverCount += 1;
-    $( this ).find( "p" ).text( "x " + mouseOverCount );
-  })
-  .mouseout(function() {
-    $( this ).find( "p" ).text( "mouse out" );
-  });
+.mouseover(function() {
+  mouseOverCount += 1;
+  $( this ).find( "p" ).text(arr[mouseOverCount %arr.length]);
+})
+.mouseout(function() {
+  $( this ).find( "p" ).text( "mouse out" );
+});
 
 // make div draggable
   $( function() {
